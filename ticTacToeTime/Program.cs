a619474b36c62a0d7c66c16a67ef99a71730c0fa
bendaley <bbdaley21@gmail.com>
2023-01-23 11:37:53 -0700

    Push time baby

--- ticTacToeTime/Program.cs
+++ ticTacToeTime/Program.cs
@@ -8,7 +8,16 @@ namespace ticTacToeTime
         {
             Console.WriteLine("Welcome to the game");
             
-            
+            Supporting sup = new Supporting();
+            char[][] tic = new char[3][3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    tic[i][j] = 'r'
+                }
+            }
+            sup.PrintBoard(tic);
         }
     }
 }
--- ticTacToeTime/Supporting.cs
+++ ticTacToeTime/Supporting.cs
@@ -3,8 +3,67 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace ticTacToeTime
-{
-    class Supporting
+{    
+    public class Supporting
     {
+        // method to print board
+        public void PrintBoard(char[,] boardArray)
+        {
+            Console.WriteLine("\t1   2   3");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("1\t" + boardArray[0,0] + " | " + boardArray[0,1] + " | " + boardArray[0,2]);
+            Console.WriteLine("\t---------");
+            Console.WriteLine("2\t" + boardArray[1,0] + " | " + boardArray[1,1] + " | " + boardArray[1,2]);
+            Console.WriteLine("\t---------");
+            Console.WriteLine("3\t" + boardArray[2,0] + " | " + boardArray[2,1] + " | " + boardArray[2,2]);
+        }
+
+        // method to return winner
+        public char Winner(char[,] boardArray)
+        {
+            char winningTeam = ' ';
+            // horizontal check
+            if ((boardArray[0,1] == boardArray[0, 2]) && (boardArray[0, 0] == boardArray[0, 2]))
+            {
+                winningTeam = boardArray[0, 0];
+            }
+            else if ((boardArray[1, 1] == boardArray[1, 2]) && (boardArray[1, 0] == boardArray[1, 2]))
+            {
+                winningTeam = boardArray[1, 0];
+            }
+            else if ((boardArray[2, 1] == boardArray[2, 2]) && (boardArray[2, 0] == boardArray[2, 2]))
+            {
+                winningTeam = boardArray[2, 0];
+            }
+
+            // vertical check
+            else if ((boardArray[0, 0] == boardArray[1, 0]) && (boardArray[0, 0] == boardArray[2, 0]))
+            {
+                winningTeam = boardArray[0, 0];
+            }
+            else if ((boardArray[1, 0] == boardArray[1, 1]) && (boardArray[1, 0] == boardArray[1, 2]))
+            {
+                winningTeam = boardArray[1, 0];
+            }
+            else if ((boardArray[2, 0] == boardArray[2, 1]) && (boardArray[2, 0] == boardArray[2, 2]))
+            {
+                winningTeam = boardArray[2, 0];
+            }
+
+            // diagonal check
+            else if ((boardArray[0, 0] == boardArray[1, 1]) && (boardArray[0, 0] == boardArray[2, 2]))
+            {
+                winningTeam = boardArray[0, 0];
+            }
+            else if ((boardArray[0, 2] == boardArray[1, 1]) && (boardArray[0, 2] == boardArray[2, 0]))
+            {
+                winningTeam = boardArray[0, 2];
+            }
+
+            // return if & who the winning team was
+            return winningTeam;
+        }
     }
 }
+
